07147e4c3030f1fd459199cd7a130c3b65a2b601
jojelupipa <jesus_38b@hotmail.com>
2019-06-06 19:05:15 +0200

    Añadidos algunos comentarios descriptivos y de dónde se debe extraer la información.

--- parser.py
+++ parser.py
@@ -20,18 +20,20 @@ sinSaltos = list(map(lambda x: x.rstrip('\n'), sinSaltos))
 for i in range(0, len(sinSaltos)):
     sinSaltos[i] = sinSaltos[i].lower()
 
+# Procesamos el contenido del fichero. Las variables las metemos en un
+# string con el resto de variables y las cláusulas como strings en una lista
 listaStringClausulas = []
 for linea in sinSaltos:
     if linea.find("variables:") != -1:
         indice = linea.find(":")
         stringVariables = linea[indice + 1:]
-        # Borramos los primeros espacios si ls hubiera
+        # Borramos los primeros espacios si los hubiera
         while stringVariables[0] == ' ':
             stringVariables = stringVariables[1:]
     elif linea.find("clausulas") == -1:
         listaStringClausulas.append(linea)
 
-# Obtenemos una lista de strings para las variables 
+# Obtenemos una lista de strings para las variables
 variable = ""
 listaVariables = []
 for caracter in stringVariables:
@@ -44,14 +46,15 @@ for caracter in stringVariables:
 listaVariables.append(variable)
 
 # Formateamos correctamente el string de la lista de cláusulas
+# (eliminamos los "or")
 for i in range(0, len(listaStringClausulas)):
     while listaStringClausulas[i].find("or") != -1:
         listaStringClausulas[i] = listaStringClausulas[i].replace("or", "")
 
-#print(listaStringClausulas)
+
+# Pasamos los strings de las cláusulas a una lista de objetos de la clase Clausula
 listaLiterales = []
 listaClausulas = []
-
 for string in listaStringClausulas:
     literal = ""
     for c in string:
@@ -63,11 +66,25 @@ for string in listaStringClausulas:
     listaLiterales.append(literal)
     listaClausulas.append(Clausula(listaLiterales[0], listaLiterales[1], listaLiterales[2]))
     listaLiterales = []
-        
 
+
+# Muestra resultados
+print("Variables:")
 print(listaVariables)
-
+print("\nCláusulas:")
 for clausula in listaClausulas:
     print(clausula.l1 + " " + clausula.l2 + " " + clausula.l3)
 
-    
+
+################ USO ###########################
+#                                              #
+#  En la lista "listaLiterales" se encuentran  #
+#  todas las cláusulas que se han introducido  #
+#  en el fichero de entrada. Para trabajar con #
+#  el grafo se pueden extraer de aquí estas    #
+#  cláusulas.                                  #
+#                                              #
+#  Para obtener las variables se puede acceder #
+#  a la lista "listaVariables"                 #
+#                                              #
+################################################
